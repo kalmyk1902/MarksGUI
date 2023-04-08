150a24f54dd7f337c7a613ca0e129e3ac224ab50
kalmyk1902 <90346486+kalmyk1902@users.noreply.github.com>
2023-04-08 22:37:29 +0300

    Update RECOVER_LANG.bat

--- etc/RECOVER_LANG.bat
+++ etc/RECOVER_LANG.bat
@@ -4,4 +4,4 @@ if not exist ..\localization\ (
     mkdir ..\localization\
 )
 
-curl -o .\localization\locale.xml https://raw.githubusercontent.com/kalmyk1902/MarksGUI/main/localization/locale.xml -OL
+curl -o ..\localization\locale.xml https://raw.githubusercontent.com/kalmyk1902/MarksGUI/main/localization/locale.xml -OL
